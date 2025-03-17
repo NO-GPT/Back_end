58e746c386c69787c33c888aefc251012004f8b9
SeungHwan <silofn523@gmail.com>
2025-03-18 00:47:16 +0900

    Add files via upload

---
+++ src/test/java/com/example/new_portfolio_server/NewPortfolioServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.new_portfolio_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewPortfolioServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

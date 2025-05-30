242895dde44cd43c4809a4b06b9ccaecd387ccfb
silofn523 <silofn523@gmail.com>
2025-05-30 09:17:10 +0900

    [Feature] #19 - 북마크 내림차순 포트폴리오 정렬
    변경 사항
    - 북마크 높은순으로 포폴 정렬 기능 추가 (도메인 로직, 비즈니스 로직)
    - 사용자 프로필 조회시, 자신이 단 댓글 정보 포함
    
    기타 참고 사항
    - 관련 이슈:  19

--- src/main/java/com/example/new_portfolio_server/board/BoardController.java
+++ src/main/java/com/example/new_portfolio_server/board/BoardController.java
@@ -1,9 +1,17 @@
 package com.example.new_portfolio_server.board;
 
 import com.example.new_portfolio_server.board.dto.BoardDto;
+import com.example.new_portfolio_server.board.dto.ResponseBoardDto;
 import com.example.new_portfolio_server.board.dto.UpdateBoardDto;
 import com.example.new_portfolio_server.board.entity.Portfolio;
 import com.example.new_portfolio_server.common.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -20,6 +28,7 @@ import java.util.Map;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/portfolio")
+@Tag(name = "Portfolio API", description = "포트폴리오 관련 API")
 public class BoardController {
     private final PortfolioRepository portfolioRepository;
     private final BoardService boardService;
@@ -63,6 +72,20 @@ public class BoardController {
     }
 
     // 게시
+    @Operation(summary = "포트폴리오 생성", description = "포폴을 생성합니다",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "200",
+                    description = "포트폴리오 생성 성공",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "401",
+                    description = "인증되지 않은 사용자",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiResponse.class)))
+    })
     @PostMapping
     public ApiResponse<?> createPortfolio(
             @ModelAttribute @Valid BoardDto boardDto,
@@ -86,18 +109,50 @@ public class BoardController {
     }
 
     // id값으로 게시글 조회
+    @Operation(summary = "상세 포트폴리오 조회", description = "상세 조회합니다.",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "200",
+                    description = "포트폴리오 조회 성공",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "404",
+                    description = "포트폴리오가 존재하지 않음",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiResponse.class)))
+    })
     @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse<Portfolio>> getPortfolioById(@PathVariable Long id) {
+    public ResponseEntity<ApiResponse<ResponseBoardDto>> getPortfolioById(
+            @Parameter(description = "포폴 아이디", example = "1") @PathVariable Long id) {
         if (!portfolioRepository.existsById(id)) {
             return ResponseEntity
                     .status(HttpStatus.NOT_FOUND)
                     .body(ApiResponse
                             .error("포트폴리오가 존재하지 않습니다."));
         }
-        Portfolio portfolio = boardService.getPortfolioById(id);
+        ResponseBoardDto portfolio = boardService.getPortfolioById(id);
         return ResponseEntity
                 .ok(ApiResponse
                         .success(portfolio));
+    }
+
+    @Operation(summary = "모든 포트폴리오 조회", description = "북마크 수 기준 내림차순으로 모든 포트폴리오를 페이징하여 조회합니다.",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "200",
+                    description = "포트폴리오 조회 성공",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ApiResponse.class))),
+    })
+    @GetMapping("/top-bookmarked")
+    public ResponseEntity<ApiResponse<List<ResponseBoardDto>>> getTopBookmarkedPortfolios(
+            @Parameter(description = "페이지 번호", example = "1") @RequestParam(value = "page", defaultValue = "1") int page,
+            @Parameter(description = "페이지당 항목 수", example = "20") @RequestParam(value = "size", defaultValue = "20") int size) {
+        List<ResponseBoardDto> portfolios = boardService.getTopBookmarkedPortfolios(page, size);
+        return ResponseEntity.ok(ApiResponse.success(portfolios));
     }
 
     // id값으로 수정

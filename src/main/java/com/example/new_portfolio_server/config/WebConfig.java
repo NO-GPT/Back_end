5695496198ff10f2b803c3af31bbf7bfd1446b11
silofn523 <silofn523@gmail.com>
2025-06-09 20:34:58 +0900

    [chore] - cors문제 해결 cofnig파일 추가
    변경 사항
    - WebConfig파일 추가
    - ALB및 EC2 퍼블릭 ip, localhost:8080에 대한 접근 허용
    - 토큰 인증 경로 허용

---
+++ src/main/java/com/example/new_portfolio_server/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.new_portfolio_server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "http://2025-321-alb-432327716.ap-northeast-2.elb.amazonaws.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true); // 토큰 인증 경로 허용
+    }
+}

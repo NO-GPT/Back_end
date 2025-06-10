9267142552389f13d4d91ec0d4cf3caefbc82eab
silofn523 <silofn523@gmail.com>
2025-06-11 08:55:02 +0900

    [fix] - webConfig에 새로운 도메인 추가
    변경사항
    - 새로운 경로 추가

--- src/main/java/com/example/new_portfolio_server/config/WebConfig.java
+++ src/main/java/com/example/new_portfolio_server/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://2025-321-alb-432327716.ap-northeast-2.elb.amazonaws.com, http://3.34.127.226:8080")
+                .allowedOrigins("http://localhost:8080", "http://2025-321-alb-432327716.ap-northeast-2.elb.amazonaws.com, http://3.34.127.226:8080, https://share-folio.kro.kr")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true); // 토큰 인증 경로 허용
